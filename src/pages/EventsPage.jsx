3c1ca6f761291ce4a7512db84b56ceeb7eacf03d
Chantal Verschueren <cverschueren34@gmail.com>
2023-09-08 14:06:13 +0200

    Add loader and details

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -1,30 +1,35 @@
 import React from "react";
-import { useEffect } from "react";
-import { useState } from "react";
+
+import { useLoaderData } from "react-router-dom";
+
+export const loader = async () => {
+  const events = await fetch("http://localhost:3000/events");
+  const categories = await fetch("http://localhost:3000/categories");
+
+  return { events: await events.json(), categories: await categories.json() };
+};
 
 export const EventsPage = () => {
-  const [eventsList, setEventsList] = useState([]);
-
-  useEffect(() => {
-    async function fetchData() {
-      const response = await fetch("http://localhost:3000/events");
-      const events = await response.json();
-      setEventsList(events);
-    }
-    fetchData();
-  }, []);
-
+  const { events, categories } = useLoaderData();
   return (
     <div className="events-list">
       <h1>List of events</h1>
       <ul>
-        {eventsList.map((event) => (
+        {events.map((event) => (
           <div key={event.id}>
             <img src={event.image} />
             <h2>{event.title}</h2>
             <p>{event.description}</p>
             <p>Start time: {event.startTime}</p>
             <p>End time: {event.endTime}</p>
+            <p>
+              Categories:
+              {
+                categories.find((category) =>
+                  event.categoryIds.includes(category.id)
+                ).name
+              }
+            </p>
           </div>
         ))}
       </ul>
