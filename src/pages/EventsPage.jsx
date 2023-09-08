72c88bb0cb2116846a2287bcae8fc18b85e9c925
Chantal Verschueren <cverschueren34@gmail.com>
2023-09-08 12:03:32 +0200

    Fetch and display list of events

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -1,6 +1,28 @@
-import React from 'react';
-import { Heading } from '@chakra-ui/react';
+import React from "react";
+import { Heading } from "@chakra-ui/react";
+import { useEffect } from "react";
+import { useState } from "react";
 
 export const EventsPage = () => {
-  return <Heading>List of events</Heading>;
+  const [eventsList, setEventsList] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("http://localhost:3000/events");
+      const events = await response.json();
+      setEventsList(events);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div className="events-list">
+      <Heading>List of events</Heading>
+      <ul>
+        {eventsList.map((event) => (
+          <li key={event.id}>{event.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
