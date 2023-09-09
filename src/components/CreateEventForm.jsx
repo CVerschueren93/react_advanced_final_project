a222d21af6f693a6bee97886568b1b1f76c891b8
Chantal Verschueren <cverschueren34@gmail.com>
2023-09-09 17:14:03 +0200

    Add handlers

--- src/components/CreateEventForm.jsx
+++ src/components/CreateEventForm.jsx
@@ -1,21 +1,100 @@
 import React from "react";
 import { Form } from "react-router-dom";
+import { useState } from "react";
 
 export const CreateEventForm = () => {
+  const [eventTitle, setEventTitle] = useState();
+  const [eventDescription, setEventDescription] = useState();
+  const [eventImage, setEventImage] = useState();
+  const [eventCategories, setEventCategories] = useState();
+  const [eventLocation, setEventLocation] = useState();
+  const [eventStartTime, setEventStartTime] = useState();
+  const [eventEndTime, setEventEndTime] = useState();
+  const [eventUser, setEventUser] = useState();
+
+  const handleTitleChange = (event) => {
+    setEventTitle(event.target.value);
+  };
+
+  const handleDescriptionChange = (event) => {
+    setEventDescription(event.target.value);
+  };
+
+  const handleImageChange = (event) => {
+    setEventImage(event.target.value);
+  };
+
+  const handleCategoriesChange = (event) => {
+    setEventCategories(event.target.value);
+  };
+
+  const handleLocationChange = (event) => {
+    setEventLocation(event.target.value);
+  };
+
+  const handleStartTimeChange = (event) => {
+    setEventStartTime(event.target.value);
+  };
+
+  const handleEndTimeChange = (event) => {
+    setEventEndTime(event.target.value);
+  };
+
+  const handleUserChange = (event) => {
+    setEventUser(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newEvent = {
+      eventTitle,
+      eventDescription,
+      eventImage,
+      eventCategories,
+      eventLocation,
+      eventStartTime,
+      eventEndTime,
+      eventUser,
+    };
+
+    fetch("http://localhost:3000/events", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newEvent),
+    });
+  };
+
   return (
     <div className="new-event">
-      <Form method="post">
+      <Form method="post" onSubmit={handleSubmit}>
         <label>
           <h2>Title</h2>
-          <input type="text" name="title" placeholder="Title" />
+          <input
+            type="text"
+            name="title"
+            placeholder="Title"
+            value={eventTitle}
+            onChange={handleTitleChange}
+          />
         </label>
         <label>
           <h2>Description</h2>
-          <textarea name="description" placeholder="Desription" />
+          <textarea
+            name="description"
+            placeholder="Desription"
+            value={eventDescription}
+            onChange={handleDescriptionChange}
+          />
         </label>
         <label>
           <h2>Image</h2>
-          <input type="text" name="URL" placeholder="ImageURL" />
+          <input
+            type="text"
+            name="URL"
+            placeholder="ImageURL"
+            value={eventImage}
+            onChange={handleImageChange}
+          />
         </label>
         <h2>Categories</h2>
         <input type="checkbox" id="sports" name="sports" value="Sports" />
@@ -31,20 +110,52 @@ export const CreateEventForm = () => {
         <label htmlFor="relaxation">Relaxation</label>
         <label>
           <h2>Location</h2>
-          <input type="text" name="location" placeholder="Location" />
+          <input
+            type="text"
+            name="location"
+            placeholder="Location"
+            value={eventLocation}
+            onChange={handleLocationChange}
+          />
         </label>
         <label>
           <h2>Start Time</h2>
-          <input type="text" name="start-time" placeholder="Start Time" />
+          <input
+            type="text"
+            name="start-time"
+            placeholder="Start Time"
+            value={eventStartTime}
+            onChange={handleStartTimeChange}
+          />
         </label>
         <label>
           <h2>End Time</h2>
-          <input type="text" name="end-time" placeholder="End Time" />
+          <input
+            type="text"
+            name="end-time"
+            placeholder="End Time"
+            value={eventEndTime}
+            onChange={handleEndTimeChange}
+          />
         </label>
         <label>
           <h2>User</h2>
-          <input type="radio" value="Ignacio Doe" name="name" /> Ignacio Doe
-          <input type="radio" value="Jane Bennett" name="name" /> Jane Bennett
+          <input
+            type="radio"
+            value="Ignacio Doe"
+            name="name"
+            value={eventUser}
+            onChange={handleUserChange}
+          />{" "}
+          Ignacio Doe
+          <input
+            type="radio"
+            value="Jane Bennett"
+            name="name"
+            value={eventUser}
+            onChange={handleUserChange}
+          />{" "}
+          Jane Bennett
         </label>
         <button type="submit">Add event</button>
       </Form>
