da769dcce077b26e96e1e8670f86b96b05cc5dd9
Chantal Verschueren <cverschueren34@gmail.com>
2023-09-11 14:08:35 +0200

    Create useEffect hook,  create createEvent function and pass this as prop to CreateEventForm inside Popup component

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import { useLoaderData } from "react-router-dom";
 import { Popup } from "../components/Popup";
 import { useState } from "react";
+import { CreateEventForm } from "../components/CreateEventForm";
+import { useEffect } from "react";
 
 export const loader = async () => {
   const events = await fetch("http://localhost:3000/events");
@@ -14,12 +16,34 @@ export const loader = async () => {
 export const EventsPage = () => {
   const [buttonPopup, setButtonPopup] = useState(false);
   const { events, categories } = useLoaderData();
+  const [allEvents, setAllEvents] = useState([]);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      const response = await fetch("http://localhost:3000/events");
+      const listOfEvents = await response.json();
+      setAllEvents(listOfEvents);
+    };
+    fetchEvents();
+  }, []);
+
+  const createEvent = async (event) => {
+    const response = await fetch("http://localhost:3000/events", {
+      method: "POST",
+      body: JSON.stringify(event),
+      headers: { "Content-Type": "application/json;charset=utf-8" },
+    });
+    event.id = (await response.json()).id;
+    setAllEvents(allEvents.concat(event));
+  };
+
   return (
     <div className="events-list">
       <h1>List of events</h1>
       <button onClick={() => setButtonPopup(true)}>Add Event</button>
       <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
         <h3>Create Event</h3>
+        <CreateEventForm createEvent={createEvent} />
       </Popup>
       <ul>
         {events.map((event) => (
