8e1de120e6682ab1a70ebdd31cb6000ca4059925
Chantal Verschueren <cverschueren34@gmail.com>
2023-09-11 14:07:12 +0200

    Pass parameter, update handleSubmit, update JSX

--- src/components/CreateEventForm.jsx
+++ src/components/CreateEventForm.jsx
@@ -2,32 +2,40 @@ import React from "react";
 import { Form } from "react-router-dom";
 import { useState } from "react";
 
-export const CreateEventForm = () => {
-  const [state, setState] = useState({
-    title: "",
-    description: "",
-    url: "",
-    categories: {
-      sports: true,
-      games: true,
-      relaxation: true,
-    },
-    location: "",
-    startTime: "",
-    endTime: "",
-    user: "Ignacio Doe",
-  });
+export const CreateEventForm = ({ createEvent }) => {
+  const [eventTitle, setEventTitle] = useState("");
+  const [eventDescription, setEventDescription] = useState("");
+  const [eventImage, setEventImage] = useState("");
+  const [eventCategories, setEventCategories] = useState(false);
+  const [eventLocation, setEventLocation] = useState("");
+  const [eventStartTime, setEventStartTime] = useState("");
+  const [eventEndTime, setEventEndTime] = useState("");
+  const [eventUser, setEventUser] = useState(false);
 
-  function handleChange(event) {
-    const value =
-      event.target.type === "checkbox"
-        ? event.target.checked
-        : event.target.value;
-    setState({
-      ...state,
-      [event.target.name]: value,
-    });
-  }
+  // title: "",
+  //  description: "",
+  // url: "",
+  // categories: {
+  //  sports: true,
+  //  games: true,
+  //  relaxation: true,
+  // },
+  // location: "",
+  //startTime: "",
+  // endTime: "",
+  // user: "Ignacio Doe",
+  // });
+
+  // function handleChange(event) {
+  // const value =
+  //  event.target.type === "checkbox"
+  //   ? event.target.checked
+  //  : event.target.value;
+  // setState({
+  ////  ...state,
+  //  [event.target.name]: value,
+  //  });
+  // }
   // const [eventTitle, setEventTitle] = useState();
   // const [eventDescription, setEventDescription] = useState();
   // const [eventImage, setEventImage] = useState();
@@ -68,165 +76,203 @@ export const CreateEventForm = () => {
   //const handleUserChange = (event) => {
   // setEventUser(event.target.value);
   //};
-
   const handleSubmit = (event) => {
     event.preventDefault();
-    const newEvent = {
-      title,
-      description,
-      url,
-      categories,
-      location,
-      startTime,
-      endTime,
-      user,
-    };
-
-    //const handleSubmit = (event) => {
-    // event.preventDefault();
-    // const newEvent = {
-    //  eventTitle,
-    //  eventDescription,
-    //  eventImage,
-    //  eventCategories,
-    //  eventLocation,
-    //   eventStartTime,
-    //  eventEndTime,
-    //  eventUser,
-    //};
-
-    fetch("http://localhost:3000/events", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(newEvent),
+    createEvent({
+      eventTitle,
+      eventDescription,
+      eventImage,
+      eventCategories: {
+        sports: false,
+        games: false,
+        relaxation: false,
+      },
+      eventLocation,
+      eventStartTime,
+      eventEndTime,
+      eventUser: {
+        IgnacioDoe: false,
+        JaneBennett: false,
+      },
     });
-
-    return (
-      <div className="new-event">
-        <Form method="post" onSubmit={handleSubmit}>
-          <label>
-            <h2>Title</h2>
-            <input
-              type="text"
-              name="title"
-              placeholder="Title"
-              //value={eventTitle}
-              //onChange={handleTitleChange}
-              value={state.title}
-              onChange={handleChange}
-            />
-          </label>
-          <label>
-            <h2>Description</h2>
-            <textarea
-              name="description"
-              placeholder="Desription"
-              // value={eventDescription}
-              // onChange={handleDescriptionChange}
-              value={state.description}
-              onChange={handleChange}
-            />
-          </label>
-          <label>
-            <h2>Image</h2>
-            <input
-              type="text"
-              name="url"
-              placeholder="ImageURL"
-              //value={eventImage}
-              // onChange={handleImageChange}
-              value={state.url}
-              onChange={handleChange}
-            />
-          </label>
-          <h2>Categories</h2>
-          <input
-            type="checkbox"
-            id="sports"
-            name="sports"
-            value="Sports"
-            checked={state.categories.sports}
-            onChange={handleChange}
-          />
-          <label htmlFor="sports">Sports</label>
-          <input
-            type="checkbox"
-            id="games"
-            name="games"
-            value="Games"
-            checked={state.categories.games}
-            onChange={handleChange}
-          />
-          <label htmlFor="games">Games</label>
-          <input
-            type="checkbox"
-            id="relaxation"
-            name="relaxation"
-            value="Relaxation"
-            checked={state.categories.relaxation}
-            onChange={handleChange}
-          />
-          <label htmlFor="relaxation">Relaxation</label>
-          <label>
-            <h2>Location</h2>
-            <input
-              type="text"
-              name="location"
-              placeholder="Location"
-              //value={eventLocation}
-              // onChange={handleLocationChange}
-              value={state.location}
-              onChange={handleChange}
-            />
-          </label>
-          <label>
-            <h2>Start Time</h2>
-            <input
-              type="text"
-              name="startTime"
-              placeholder="Start Time"
-              // value={eventStartTime}
-              //onChange={handleStartTimeChange}
-              value={state.startTime}
-              onChange={handleChange}
-            />
-          </label>
-          <label>
-            <h2>End Time</h2>
-            <input
-              type="text"
-              name="endTime"
-              placeholder="End Time"
-              // value={eventEndTime}
-              //onChange={handleEndTimeChange}
-              value={state.endTime}
-              onChange={handleChange}
-            />
-          </label>
-          <label>
-            <h2>User</h2>
-            <input
-              type="radio"
-              name="user"
-              // value={eventUser}
-              // onChange={handleUserChange}
-              checked={state.user === "Ignacio Doe"}
-              onChange={handleChange}
-            />{" "}
-            Ignacio Doe
-            <input
-              type="radio"
-              name="user"
-              //value={eventUser}
-              // onChange={handleUserChange}
-              checked={state.user === "Jane Bennett"}
-              onChange={handleChange}
-            />{" "}
-            Jane Bennett
-          </label>
-          <button type="submit">Add event</button>
-        </Form>
-      </div>
-    );
+    setEventTitle("");
+    setEventDescription("");
+    setEventImage("");
+    setEventCategories(false);
+    setEventLocation("");
+    setEventStartTime("");
+    setEventEndTime("");
+    setEventUser(false);
   };
+
+  //const handleSubmit = (event) => {
+  // event.preventDefault();
+  // const newEvent = {
+  //  title,
+  //  description,
+  //  url,
+  /// categories,
+  // location,
+  // startTime,
+  // endTime,
+  // user,
+  // };
+
+  //const handleSubmit = (event) => {
+  // event.preventDefault();
+  // const newEvent = {
+  //  eventTitle,
+  //  eventDescription,
+  //  eventImage,
+  //  eventCategories,
+  //  eventLocation,
+  //   eventStartTime,
+  //  eventEndTime,
+  //  eventUser,
+  //};
+
+  //fetch("http://localhost:3000/events", {
+  //  method: "POST",
+  // headers: { "Content-Type": "application/json" },
+  //  body: JSON.stringify(newEvent),
+  // });
+
+  return (
+    <div className="new-event">
+      <Form method="post" onSubmit={handleSubmit}>
+        <label>
+          <h2>Title</h2>
+          <input
+            type="text"
+            name="title"
+            placeholder="Title"
+            value={eventTitle}
+            //onChange={handleTitleChange}
+            //value={state.title}
+            // onChange={handleChange}
+            onChange={(e) => setEventTitle(e.target.value)}
+          />
+        </label>
+        <label>
+          <h2>Description</h2>
+          <textarea
+            name="description"
+            placeholder="Desription"
+            value={eventDescription}
+            // onChange={handleDescriptionChange}
+            // value={state.description}
+            //onChange={handleChange}
+            onChange={(e) => setEventDescription(e.target.value)}
+          />
+        </label>
+        <label>
+          <h2>Image</h2>
+          <input
+            type="text"
+            name="url"
+            placeholder="ImageURL"
+            value={eventImage}
+            // onChange={handleImageChange}
+            //value={state.url}
+            // onChange={handleChange}
+            onChange={(e) => setEventImage(e.target.value)}
+          />
+        </label>
+        <h2>Categories</h2>
+        <input
+          type="checkbox"
+          id="sports"
+          name="sports"
+          value={eventCategories.sports}
+          // checked={state.categories.sports}
+          // onChange={handleChange}
+          onChange={(e) => setEventCategories(e.target.value)}
+        />
+        <label htmlFor="sports">Sports</label>
+        <input
+          type="checkbox"
+          id="games"
+          name="games"
+          value={eventCategories.games}
+          //checked={state.categories.games}
+          // onChange={handleChange}
+          onChange={(e) => setEventCategories(e.target.value)}
+        />
+        <label htmlFor="games">Games</label>
+        <input
+          type="checkbox"
+          id="relaxation"
+          name="relaxation"
+          value={eventCategories.relaxation}
+          //checked={state.categories.relaxation}
+          // onChange={handleChange}
+          onChange={(e) => setEventCategories(e.target.value)}
+        />
+        <label htmlFor="relaxation">Relaxation</label>
+        <label>
+          <h2>Location</h2>
+          <input
+            type="text"
+            name="location"
+            placeholder="Location"
+            value={eventLocation}
+            // onChange={handleLocationChange}
+            //value={state.location}
+            // onChange={handleChange}
+            onChange={(e) => setEventLocation(e.target.value)}
+          />
+        </label>
+        <label>
+          <h2>Start Time</h2>
+          <input
+            type="text"
+            name="startTime"
+            placeholder="Start Time"
+            value={eventStartTime}
+            //onChange={handleStartTimeChange}
+            //value={state.startTime}
+            //onChange={handleChange}
+            onChange={(e) => setEventStartTime(e.target.value)}
+          />
+        </label>
+        <label>
+          <h2>End Time</h2>
+          <input
+            type="text"
+            name="endTime"
+            placeholder="End Time"
+            value={eventEndTime}
+            //onChange={handleEndTimeChange}
+            //value={state.endTime}
+            //onChange={handleChange}
+            onChange={(e) => setEventEndTime(e.target.value)}
+          />
+        </label>
+        <label>
+          <h2>User</h2>
+          <input
+            type="radio"
+            name="IgnacioDoe"
+            value={eventUser.IgnacioDoe}
+            // onChange={handleUserChange}
+            //checked={state.user === "Ignacio Doe"}
+            // onChange={handleChange}
+            onChange={(e) => setEventUser(e.target.value)}
+          />{" "}
+          Ignacio Doe
+          <input
+            type="radio"
+            name="JaneBennett"
+            value={eventUser.JaneBennett}
+            // onChange={handleUserChange}
+            //checked={state.user === "Jane Bennett"}
+            //onChange={handleChange}
+            onChange={(e) => setEventUser(e.target.value)}
+          />{" "}
+          Jane Bennett
+        </label>
+        <button type="submit">Add event</button>
+      </Form>
+    </div>
+  );
 };
