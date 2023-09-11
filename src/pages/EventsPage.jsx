d998aad22b79509ba7d869bd3837ad3700e20647
Chantal Verschueren <cverschueren34@gmail.com>
2023-09-11 17:30:13 +0200

    Remove useEffect and createEvent

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -3,8 +3,6 @@ import { Link } from "react-router-dom";
 import { useLoaderData } from "react-router-dom";
 import { Popup } from "../components/Popup";
 import { useState } from "react";
-import { CreateEventForm } from "../components/CreateEventForm";
-import { useEffect } from "react";
 
 export const loader = async () => {
   const events = await fetch("http://localhost:3000/events");
@@ -16,26 +14,6 @@ export const loader = async () => {
 export const EventsPage = () => {
   const [buttonPopup, setButtonPopup] = useState(false);
   const { events, categories } = useLoaderData();
-  const [allEvents, setAllEvents] = useState([]);
-
-  useEffect(() => {
-    const fetchEvents = async () => {
-      const response = await fetch("http://localhost:3000/events");
-      const listOfEvents = await response.json();
-      setAllEvents(listOfEvents);
-    };
-    fetchEvents();
-  }, []);
-
-  const createEvent = async (event) => {
-    const response = await fetch("http://localhost:3000/events", {
-      method: "POST",
-      body: JSON.stringify(event),
-      headers: { "Content-Type": "application/json;charset=utf-8" },
-    });
-    event.id = (await response.json()).id;
-    setAllEvents(allEvents.concat(event));
-  };
 
   return (
     <div className="events-list">
@@ -43,7 +21,6 @@ export const EventsPage = () => {
       <button onClick={() => setButtonPopup(true)}>Add Event</button>
       <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
         <h3>Create Event</h3>
-        <CreateEventForm createEvent={createEvent} />
       </Popup>
       <ul>
         {events.map((event) => (
@@ -56,11 +33,9 @@ export const EventsPage = () => {
               <p>End time: {event.endTime}</p>
               <p>
                 Categories:
-                {
-                  categories.find((category) =>
-                    event.categoryIds.includes(category.id)
-                  ).name
-                }
+                {categories.filter((category) =>
+                  event.categoryIds.includes(category.id) ? category.name : ""
+                )}
               </p>
             </Link>
           </div>
