33385b01c38bf68e89c4be8ff7e005ccfa51721f
Chantal Verschueren <cverschueren34@gmail.com>
2023-09-15 12:15:31 +0200

    Create SearchBar component

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import React from "react";
+
+export const SearchBar = ({ setResults }) => {
+  const fetchData = (value) => {
+    fetch("http://localhost:3000/events")
+      .then((response) => response.json())
+      .then((json) => {
+        const results = json.filter((event) => {
+          return (
+            value &&
+            event &&
+            event.title &&
+            event.title.toLowerCase().includes(value)
+          );
+        });
+        setResults(results);
+      });
+  };
+
+  const handleChange = (value) => {
+    setInput(value);
+    fetchData(value);
+  };
+
+  const [input, setInput] = useState("");
+  return (
+    <div className="input-wrapper">
+      <input
+        placeholder="Type to search..."
+        value={input}
+        onChange={(e) => handleChange(e.target.value)}
+      />
+    </div>
+  );
+};
