83f2d7a57b5ea42d00410850e3e962210a389612
Chantal Verschueren <cverschueren34@gmail.com>
2023-09-15 12:15:15 +0200

    Import SearchBar & SearchResultsList

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import { useLoaderData } from "react-router-dom";
 import { Popup } from "../components/Popup";
 import { useState } from "react";
+import { SearchBar } from "../components/SearchBar";
+import { SearchResultsList } from "../components/SearchResultsList";
 
 export const loader = async () => {
   const events = await fetch("http://localhost:3000/events");
@@ -15,9 +17,15 @@ export const EventsPage = () => {
   const [buttonPopup, setButtonPopup] = useState(false);
   const { events, categories } = useLoaderData();
 
+  const [results, setResults] = useState([]);
+
   return (
     <div className="events-list">
       <h1>List of events</h1>
+      <div className="search-bar-container">
+        <SearchBar setResults={setResults} />
+        <SearchResultsList results={results} />
+      </div>
       <button onClick={() => setButtonPopup(true)}>Add Event</button>
       <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
         <h3>Create Event</h3>
