9abab87a47734a6ffa886a93ed50edf44b3d8d79
Chantal Verschueren <cverschueren34@gmail.com>
2023-09-25 14:00:12 +0200

    Create loader & update EventPage component

--- src/pages/EventPage.jsx
+++ src/pages/EventPage.jsx
@@ -1,6 +1,51 @@
-import React from 'react';
-import { Heading } from '@chakra-ui/react';
+import React from "react";
+import { Heading } from "@chakra-ui/react";
+import { useLoaderData } from "react-router-dom";
+
+export const loader = async () => {
+  const events = await fetch("http://localhost:3000/events");
+  const categories = await fetch("http://localhost:3000/categories");
+  const users = await fetch("http://localhost:3000/users");
+
+  return {
+    events: await events.json(),
+    categories: await categories.json(),
+    users: await users.json(),
+  };
+};
 
 export const EventPage = () => {
-  return <Heading>Event</Heading>;
+  const { events, categories, users } = useLoaderData();
+
+  return (
+    <div className="event-details">
+      <Heading>Event</Heading>
+      <ul>
+        {events.map((event) => (
+          <div key={event.id} className="event">
+            <img src={event.image} />
+            <h2>{event.title}</h2>
+            <p>{event.description}</p>
+            <p>Start time: {event.startTime}</p>
+            <p>End time: {event.endTime}</p>
+            <p>
+              Categories:
+              {categories
+                .filter((category) =>
+                  event.categoryIds?.includes(category.id) ? category.name : ""
+                )
+                .map((category) => " " + category.name + " ")}
+            </p>
+            <p>
+              Created by:
+              {users.find((user) => event.createdBy === user.id).name}
+              <img
+                src={users.find((user) => event.createdBy === user.id).image}
+              />
+            </p>
+          </div>
+        ))}
+      </ul>
+    </div>
+  );
 };
