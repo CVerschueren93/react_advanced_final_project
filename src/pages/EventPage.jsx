9cb048802ac462859181536e7b2189ff9b67cfb7
Chantal Verschueren <cverschueren34@gmail.com>
2023-09-26 12:10:35 +0200

    Update loader & EventPage component

--- src/pages/EventPage.jsx
+++ src/pages/EventPage.jsx
@@ -2,49 +2,47 @@ import React from "react";
 import { Heading } from "@chakra-ui/react";
 import { useLoaderData } from "react-router-dom";
 
-export const loader = async () => {
-  const events = await fetch("http://localhost:3000/events");
+export const loader = async ({ params }) => {
+  const event = await fetch("http://localhost:3000/events/${params.eventId}");
   const categories = await fetch("http://localhost:3000/categories");
   const users = await fetch("http://localhost:3000/users");
 
   return {
-    events: await events.json(),
+    event: await event.json(),
     categories: await categories.json(),
     users: await users.json(),
   };
 };
 
 export const EventPage = () => {
-  const { events, categories, users } = useLoaderData();
+  const { event, categories, users } = useLoaderData();
 
   return (
     <div className="event-details">
       <Heading>Event</Heading>
       <ul>
-        {events.map((event) => (
-          <div key={event.id} className="event">
-            <img src={event.image} />
-            <h2>{event.title}</h2>
-            <p>{event.description}</p>
-            <p>Start time: {event.startTime}</p>
-            <p>End time: {event.endTime}</p>
-            <p>
-              Categories:
-              {categories
-                .filter((category) =>
-                  event.categoryIds?.includes(category.id) ? category.name : ""
-                )
-                .map((category) => " " + category.name + " ")}
-            </p>
-            <p>
-              Created by:
-              {users.find((user) => event.createdBy === user.id).name}
-              <img
-                src={users.find((user) => event.createdBy === user.id).image}
-              />
-            </p>
-          </div>
-        ))}
+        <div key={event.id} className="event">
+          <img src={event.image} />
+          <h2>{event.title}</h2>
+          <p>{event.description}</p>
+          <p>Start time: {event.startTime}</p>
+          <p>End time: {event.endTime}</p>
+          <p>
+            Categories:
+            {categories
+              .filter((category) =>
+                event.categoryIds?.includes(category.id) ? category.name : ""
+              )
+              .map((category) => " " + category.name + " ")}
+          </p>
+          <p>
+            Created by:
+            {users.find((user) => event.createdBy === user.id).name}
+            <img
+              src={users.find((user) => event.createdBy === user.id).image}
+            />
+          </p>
+        </div>
       </ul>
     </div>
   );
