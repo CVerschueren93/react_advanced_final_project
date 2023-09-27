25e0c1e88d76eb9039e2de311c6d43e9e6fcf2da
Chantal Verschueren <cverschueren34@gmail.com>
2023-09-27 11:43:09 +0200

    Import Popup2

--- src/pages/EventPage.jsx
+++ src/pages/EventPage.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Heading } from "@chakra-ui/react";
 import { useLoaderData } from "react-router-dom";
+import { Popup2 } from "../components/Popup2";
+import { useState } from "react";
 
 export const loader = async ({ params }) => {
   const event = await fetch(`http://localhost:3000/events/${params.eventId}`);
@@ -16,10 +18,15 @@ export const loader = async ({ params }) => {
 
 export const EventPage = () => {
   const { event, categories, users } = useLoaderData();
+  const [buttonPopup, setButtonPopup] = useState(false);
 
   return (
     <div className="event-details">
       <Heading>Event</Heading>
+      <button onClick={() => setButtonPopup(true)}>Edit Event</button>
+      <Popup2 trigger={buttonPopup} setTrigger={setButtonPopup}>
+        <h3>Edit Event</h3>
+      </Popup2>
       <ul>
         <div key={event.id} className="event">
           <img src={event.image} />
