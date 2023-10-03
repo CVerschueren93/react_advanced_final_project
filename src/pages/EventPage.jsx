ab1b37fefce0f27f628282c8eb06104a2c32aeed
Chantal Verschueren <cverschueren34@gmail.com>
2023-10-03 10:52:51 +0200

    Import css page and add classnames

--- src/pages/EventPage.jsx
+++ src/pages/EventPage.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { Heading } from "@chakra-ui/react";
 import { useLoaderData } from "react-router-dom";
 import { Popup2 } from "../components/Popups/Popup2";
 import { useState } from "react";
 import { UpdateEventForm } from "../components/Forms/UpdateEventForm";
 import { Popup3 } from "../components/Popups/Popup3";
+import "./EventPage.css";
 
 export const loader = async ({ params }) => {
   const event = await fetch(`http://localhost:3000/events/${params.eventId}`);
@@ -24,18 +24,22 @@ export const EventPage = () => {
 
   return (
     <div className="event-details">
-      <Heading>Event</Heading>
-      <button onClick={() => setButtonPopup(true)}>Edit Event</button>
+      <h1>Event</h1>
+      <button className="edit-event" onClick={() => setButtonPopup(true)}>
+        Edit Event
+      </button>
       <Popup2 trigger={buttonPopup} setTrigger={setButtonPopup}>
         <h3>Edit Event</h3>
         <UpdateEventForm event={event} />
       </Popup2>
-      <button onClick={() => setButtonPopup(true)}>Delete Event</button>
+      <button className="delete-event" onClick={() => setButtonPopup(true)}>
+        Delete Event
+      </button>
       <Popup3 trigger={buttonPopup} setTrigger={setButtonPopup}></Popup3>
       <ul>
-        <div key={event.id} className="event">
-          <img src={event.image} />
-          <h2>{event.title}</h2>
+        <div key={event.id} className="event-info">
+          <img className="image" src={event.image} />
+          <h2 className="header-two">{event.title}</h2>
           <p>{event.description}</p>
           <p>Start time: {event.startTime}</p>
           <p>End time: {event.endTime}</p>
@@ -51,6 +55,7 @@ export const EventPage = () => {
             Created by:
             {users.find((user) => event.createdBy === user.id)?.name}
             <img
+              className="image-user"
               src={users.find((user) => event.createdBy === user.id)?.image}
             />
           </p>
