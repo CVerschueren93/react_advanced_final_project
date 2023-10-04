c3ef8bec9efa04e666a16a363e1e90da98ae9a46
Chantal Verschueren <cverschueren34@gmail.com>
2023-10-04 13:41:33 +0200

    Update Popup3

--- src/components/Popups/Popup3.jsx
+++ src/components/Popups/Popup3.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import "./Popup3.css";
-import { DeleteEvent } from "../DeleteEvent";
+import { useNavigate } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
 
 export function Popup3(props) {
+  const navigate = useNavigate();
+  const loader = async ({ params }) => {
+    const event = await fetch(`http://localhost:3000/events/${params.eventId}`);
+
+    return {
+      event: await event.json(),
+    };
+  };
+  const DeleteEvent = async () => {
+    const { event } = useLoaderData();
+    const response = await fetch(`http://localhost:3000/events/${event.id}`, {
+      method: "DELETE",
+    });
+    if (response.ok) {
+      navigate(`/events`);
+    }
+  };
+
   return props.trigger ? (
     <div className="popup3">
       <div className="popup3-inner">
@@ -14,7 +33,7 @@ export function Popup3(props) {
         </button>
         {props.children}
         <p>Are you sure you want to delete this event?</p>
-        <button onClick={DeleteEvent()}>Confirm</button>
+        <button onClick={() => DeleteEvent()}>Confirm</button>
       </div>
     </div>
   ) : (
